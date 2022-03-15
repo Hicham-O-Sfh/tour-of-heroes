5b9131d58d89497c5c153736d4672377aefe5975
Hicham O-Sfh <Hicham.O.Sfh@gmail.com>
2022-03-15 15:45:15 +0100

    utilisation de l'interpolation et des events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app-component',
   template: `
-  <h1>Ceci est un test</h1>
+  <h1>{{title + ' !'}}</h1>
   <h3>
     <u>
       Modification direct du template
@@ -13,12 +13,34 @@ import { Component } from '@angular/core';
     Ceci est du HTML intégré au sein du 'Template' attribute, cela est une assez mauvaise pratique, il est préférable de mettre la structuration HTML au sein
     d'un fichier dédié mon-component.component.html, et de l'interpelé au sein de 'TemplateURL' du décorateur @Component !
   </p>
-  <small style="margin-left: 20%;">
+  <button  (click)="thanks()" #click_me_button>
+    WHO AM I !!
+  </button>
+  <br>
+  <small [ngStyle]="{'margin-left': 20 + '%'}" (mouseenter)="mouseOverMe($event)">
     Merci à SQLI, et à mon formateur -Mr. Hicham Maaqoul-
   </small>
+  <section id="the_section" #the_section>
+    nom complet : {{ nomComplet }}
+    <br>
+    Age : {{ age }} ans
+    <br>
+    Satifaction : {{ isHappy ? "✅" : "❌" }}
+  </section>
   `,
-  styles: ['h1,small {background: blue; color: white; text-align:center;}']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'tour-of-heroes';
+  title: string = 'Text from the {{ interpollation }}';
+  nomComplet: string = "Hicham Oussama Saffih";
+  age: number = 24;
+  isHappy: Boolean = true;
+
+  thanks() {
+    alert("I'm -Hicham O'Sfh- an e-challenger !!");
+  }
+
+  mouseOverMe(event: any) {
+    alert('👋You Hovered the <small> tag !\nThe text of the <small> tag is : ' + event.target.innerHTML);
+  }
 }
